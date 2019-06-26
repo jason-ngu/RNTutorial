bb434e6e9ee40a655ae3c702dc9336a247cefb74
Jason Ngu <jason.ngu99@gmail.com>
2019-06-26 09:55:46 -0700

    Completed login handler for Datalogging

--- Header.js
+++ Header.js
@@ -3,30 +3,32 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const Header = (props) => {
     return (
-        <View style = {styles.container}>
+        <View style={styles.container}>
             <Text style={styles.tcontainer}>{props.headerText}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F8F8F8',
-    paddingTop: 20,
-    height: 60,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2},
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative',
-  },
-  tcontainer: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: 'red',
-  },
+    container: {
+        alignItems: 'center',   // vertical
+        justifyContent: 'center',   // horizontal
+        backgroundColor: '#F8F8F8',
+        paddingTop: 20,
+        height: 64,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative',
+        marginBottom: 50,
+    },
+    tcontainer: {
+        justifyContent: 'center',
+        fontSize: 64,
+        fontWeight: 'bold',
+        color: 'black',
+    },
 })
 
 export default Header;
